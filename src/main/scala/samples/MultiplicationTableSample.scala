b12909a73f9d513c2c65490ade3a21cd2e9857a9
Victor Perepelitsky <victor.prp@gmail.com>
2016-01-21 00:12:13 +0200

    make it compile

--- src/main/scala/samples/MultiplicationTableSample.scala
+++ src/main/scala/samples/MultiplicationTableSample.scala
@@ -5,43 +5,43 @@ package samples
  */
 object MultiplicationTableSample extends App {
 
-  val staticTable = List("  1", "  2" , "  3", "\n",
-                         "  2", "  4" , "  6", "\n",
-                         "  3", "  6" , "  9", "\n")
-
-
-
-    printTable(staticTable)
-
-
-  def prettyStringFrom(result: Int):String = {
-    "a"
-  }
-
-  def createRow(rowNumber: Int, size: Int):String = {
-    val row =
-      for {
-        item <- 1 to size
-        result = rowNumber * item
-      }yield prettyStringFrom(result)
-
-    val finalRow = row.toList
-    finalRow.
-  }
-
-  def createMultiTableOf(size:Int):List[String]={
-    val table =
-    for{
-      rowNumber <- 1 to size
-    }yield createRow(rowNumber,size)
-
-    table toList
-  }
-
-
-  def printTable(table: List[String]) {
-    for {
-      item <- table
-    }print(item)
-  }
+//  val staticTable = List("  1", "  2" , "  3", "\n",
+//                         "  2", "  4" , "  6", "\n",
+//                         "  3", "  6" , "  9", "\n")
+//
+//
+//
+//    printTable(staticTable)
+//
+//
+//  def prettyStringFrom(result: Int):String = {
+//    "a"
+//  }
+//
+//  def createRow(rowNumber: Int, size: Int):String = {
+//    val row =
+//      for {
+//        item <- 1 to size
+//        result = rowNumber * item
+//      }yield prettyStringFrom(result)
+//
+//    val finalRow = row.toList
+//    finalRow.
+//  }
+//
+//  def createMultiTableOf(size:Int):List[String]={
+//    val table =
+//    for{
+//      rowNumber <- 1 to size
+//    }yield createRow(rowNumber,size)
+//
+//    table toList
+//  }
+//
+//
+//  def printTable(table: List[String]) {
+//    for {
+//      item <- table
+//    }print(item)
+//  }
 }
