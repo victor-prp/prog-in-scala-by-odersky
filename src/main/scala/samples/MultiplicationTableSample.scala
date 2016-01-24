a7fe34aa6c7275e6459305f3c8822771555aca58
Victor Perepelitsky <Victor.Perepelitsky@netapp.com>
2016-01-24 17:17:10 +0200

    multitable works

--- src/main/scala/samples/MultiplicationTableSample.scala
+++ src/main/scala/samples/MultiplicationTableSample.scala
@@ -5,43 +5,48 @@ package samples
  */
 object MultiplicationTableSample extends App {
 
-//  val staticTable = List("  1", "  2" , "  3", "\n",
-//                         "  2", "  4" , "  6", "\n",
-//                         "  3", "  6" , "  9", "\n")
-//
-//
-//
-//    printTable(staticTable)
-//
-//
-//  def prettyStringFrom(result: Int):String = {
-//    "a"
-//  }
-//
-//  def createRow(rowNumber: Int, size: Int):String = {
-//    val row =
-//      for {
-//        item <- 1 to size
-//        result = rowNumber * item
-//      }yield prettyStringFrom(result)
-//
-//    val finalRow = row.toList
-//    finalRow.
-//  }
-//
-//  def createMultiTableOf(size:Int):List[String]={
-//    val table =
-//    for{
-//      rowNumber <- 1 to size
-//    }yield createRow(rowNumber,size)
-//
-//    table toList
-//  }
-//
-//
-//  def printTable(table: List[String]) {
-//    for {
-//      item <- table
-//    }print(item)
-//  }
+  val staticTable = List("  1", "  2", "  3", "\n",
+    "  2", "  4", "  6", "\n",
+    "  3", "  6", "  9", "\n")
+
+
+  printTable(staticTable)
+
+  printTable(createMultiTableOf(10))
+
+  def prettyStringFrom(num: Int): String = {
+    val str = "    " + num
+    val toRemove = str.length - 4;
+    str.substring(toRemove)
+  }
+
+  def createRow(rowNumber: Int, size: Int): List[String] = {
+    val row =
+      for {
+        item <- 1 to size
+        result = rowNumber * item
+      } yield prettyStringFrom(result)
+
+    val rowAsList = row.toList
+    rowAsList ::: List("\n")
+  }
+
+  def createMultiTableOf(size: Int): List[String] = {
+    val allRows =
+      for {
+        rowNumber <- 1 to size
+      } yield createRow(rowNumber, size)
+
+    val emptyList: List[String] = List()
+    allRows.foldLeft(emptyList) {
+      (prev, curr) => prev ::: curr
+    }
+  }
+
+
+  def printTable(table: List[String]) {
+    for {
+      item <- table
+    } print(item)
+  }
 }
