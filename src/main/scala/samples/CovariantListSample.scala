65f6edd168c2317ee26a8910c7153a020dd4ff3b
Victor Perepelitsky <Victor.Perepelitsky@netapp.com>
2016-02-02 14:44:58 +0200

    CovariantListSample

---
+++ src/main/scala/samples/CovariantListSample.scala
@@ -0,0 +1,19 @@
+package samples
+
+import org.victor.caseclass.arithmetic.{Expr, Var, Number}
+
+/**
+ * @author victorp
+ */
+object CovariantListSample extends App{
+
+  val numbers:List[Number] = List(Number(1), Number(2))
+  val vars:List[Var] = List(Var("N"), Var("M"))
+
+
+  //Here we see how easilly namber and vars can be concatenated and assigned to more general list
+  //As opposite to Java where Collections are NOT covariant
+  val expressions:List[Expr] = numbers:::vars
+
+  println(expressions)
+}
